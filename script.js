f8542a7113b75360f8d3f4a3d15bd3ae51c0bb26
Facu Pestana <225961206+facupestana-dev@users.noreply.github.com>
2025-09-11 12:48:54 -0300

    .
    quedo Perfecta

---
+++ script.js
@@ -0,0 +1,34 @@
+// Fade-in al hacer scroll
+const faders = document.querySelectorAll('.fade-in');
+
+function checkFade() {
+  const triggerBottom = window.innerHeight * 0.95;
+
+  faders.forEach(fader => {
+    const top = fader.getBoundingClientRect().top;
+    if(top < triggerBottom) {
+      fader.classList.add('show');
+    } else {
+      fader.classList.remove('show');
+    }
+  });
+}
+
+window.addEventListener('scroll', checkFade);
+window.addEventListener('load', checkFade);
+
+// Preloader
+window.addEventListener('load', () => {
+  const preloader = document.getElementById('preloader');
+  const mainContent = document.getElementById('main-content');
+
+  // Mantener la pre-carga por 3 segundos
+  setTimeout(() => {
+    preloader.style.opacity = '0'; // fade out
+    preloader.style.pointerEvents = 'none'; 
+    setTimeout(() => {
+      preloader.style.display = 'none'; // desaparece totalmente
+      mainContent.style.opacity = '1';  // muestra contenido principal
+    }, 500); 
+  }, 3000); 
+});
